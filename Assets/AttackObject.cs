7326ff368fe6827061726a221b338ad8ee12bd35
Yuto Michishita <y-michishita@i-enter.co.jp>
2019-04-12 12:20:03 +0900

    StepAsideUnityChanプロジェクト　初めてのコミット lesson6を見ながら作成したファイルです。

---
+++ Assets/AttackObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackObject : MonoBehaviour {
+
+	public GameObject unityChanControll;
+	private Transform unitytrans;
+	// Use this for initialization
+	void Start () {
+		unityChanControll = GameObject.Find ("unitychan");
+		unitytrans = unityChanControll.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.position = new Vector3 (0,
+			0,unitytrans.transform.position.z-10);
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "CarTag") {
+			Destroy (other.gameObject);
+		}
+		else if(other.gameObject.tag=="CoinTag"){
+				Destroy (other.gameObject);
+			}
+		else if(other.gameObject.tag=="TrafficConeTag"){
+					Destroy (other.gameObject);
+			}
+		}
+
+}
